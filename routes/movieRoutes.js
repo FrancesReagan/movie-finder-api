10ce3fa065d73525bbc64c10c436f3d909c256ea
Frances Reagan James <adtgwen@gmail.com>
2025-07-11 13:39:13 -0400

    added route for searching for movies and route for searching for movies by id in movieRoutes.js

--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -3,3 +3,15 @@
 const express = require("express");
 
 // import controller functions//
+const { searchMovies, getMoviesDetails } = require("../controllers/movieControllers");
+
+// create router//
+const router = express.Router();
+
+// route for searching movies//
+router.get("/search", searchMovies);
+// route for getting movie details by ID//
+router.get("/movies/:id", getMoviesDetails);
+
+// export router//
+module.exports = router;
